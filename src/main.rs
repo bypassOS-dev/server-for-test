7f88ebef74cd0d26b68d8e5ea2ef368b7938844c
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-17 00:50:24 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
     let connect = TcpListener::bind("127.0.0.1:8080").await?;
@@ -8,23 +8,23 @@ async fn main() -> std::io::Result<()>{
         tokio::spawn(async move {
             let mut buffer = [0u8; 1024];
 
-            //let mut some = String::new();
-            //std::io::stdin()
-            //    .read_line(&mut some)
-            //    .expect("FATAL Error read of string");
-
-            let read_bytes = match socet.read(&mut buffer).await {
-                Ok(0) => {
-                    println!("A client is completed a session!");
+            loop {
+                let read_bytes = match socet.read(&mut buffer).await {
+                    Ok(0) => {
+                        println!("A client is completed a session!");
+                        return;
+                    }
+                    Ok(n) => n,
+                    Err(err) => {
+                        println!("User read error: {err}");
+                        return;
+                    }
+                };
+                if let Err(e) = socet.write_all(&buffer[..read_bytes]).await {
+                    println!("Write error from {}: {}", addr, e);
+                    return;
                 }
-                Ok(n) => n,
-                Err(err) => {
-                    println!("User read error: {err}")
-                }
-            };
-
+            }
         });
     }
-    Ok(())
 }
-
